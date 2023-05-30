59941d48318ffbc59283b4aad1a00df960648a97
Vukašin Lekić <goksi1312@gmail.com>
2023-05-30 15:14:40 +0200

    Initial

---
+++ TVP_Projekat_2/Forms/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Restoran.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
