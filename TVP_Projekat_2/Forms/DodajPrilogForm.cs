d90da9383bbe1dc04d674c2a5586c13d68a1fc72
Vukašin Lekić <goksi1312@gmail.com>
2023-06-01 19:26:27 +0200

    AAAAAAAAAAA

---
+++ TVP_Projekat_2/Forms/DodajPrilogForm.cs
@@ -0,0 +1,37 @@
+﻿using Restoran.Storage;
+using System;
+using System.Windows.Forms;
+
+namespace Restoran.Forms
+{
+    public partial class DodajPrilogForm : Form
+    {
+        private readonly IDataStorage storage;
+        public DodajPrilogForm(IDataStorage storage)
+        {
+            InitializeComponent();
+            this.storage = storage;
+        }
+
+        private void HandleClick(object sender, EventArgs e)
+        {
+            string naziv = nazivTb.Text;
+            if (naziv.Length < 3)
+            {
+                MessageBox.Show("Naziv mora imati najmanje 3 karaktera !", "Upozorenje", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!int.TryParse(cenaTb.Text, out int cena) || cena < 0)
+            {
+                MessageBox.Show("Cena mora biti pozitivan ceo broj !", "Upozorenje", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            int result = storage.DodajPrilog(naziv, cena);
+            if (result != -1)
+            {
+                MessageBox.Show("Uspesno ste dodali novi prilog !", "Uspeh !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+        }
+    }
+}
