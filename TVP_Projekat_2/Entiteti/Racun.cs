445c20441604729b3e25afe4e3ee1e4902bb84d6
Vukašin Lekić <goksi1312@gmail.com>
2023-06-01 19:26:20 +0200

    asdsd

---
+++ TVP_Projekat_2/Entiteti/Racun.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Restoran.Entiteti
+{
+    public class Racun
+    {
+        private int id;
+        private DateTime date;
+        private readonly IDictionary<StavkaRacuna, int> jela; // Stavka, kolicina
+
+        public Racun(int id, DateTime date) : this()
+        {
+            this.id = id;
+            this.date = date;
+        }
+
+        public Racun()
+        {
+            jela = new Dictionary<StavkaRacuna, int>();
+        }
+
+        public void DodajStavku(Jelo jelo, Prilog prilog)
+        {
+            StavkaRacuna stavka = new StavkaRacuna(jelo, prilog);
+            jela.TryGetValue(stavka, out int trenutnoJela);
+            jela[stavka] = trenutnoJela + 1;
+        }
+
+        public void ObrisiStavku(StavkaRacuna stavka)
+        {
+            jela.TryGetValue(stavka, out int trenutnoJela);
+            if (trenutnoJela == 1) jela.Remove(stavka);
+            else jela[stavka] = trenutnoJela - 1;
+           
+        }
+
+
+        public int Id { get => id; set => id = value; }
+
+        public DateTime Date { get => date; set => date = value; }
+
+        public IDictionary<StavkaRacuna, int> Jela => jela;
+
+        public int UkupnaCena => jela.Keys.Select(k => k.UkupnaCena * jela[k]).Sum();
+    }
+}
---
+++ TVP_Projekat_2/Entiteti/StavkaRacuna.cs
@@ -0,0 +1,49 @@
+﻿namespace Restoran.Entiteti
+{
+    public class StavkaRacuna
+    {
+        private readonly Jelo jelo;
+        private readonly Prilog prilog;
+
+        public StavkaRacuna(Jelo jelo, Prilog prilog)
+        {
+            this.jelo = jelo;
+            this.prilog = prilog;
+        }
+
+        public int UkupnaCena => CenaJelo + CenaPrilog;
+
+        public int CenaJelo => jelo.Cena;
+
+        public int CenaPrilog => prilog != null ? prilog.Cena : 0;
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            return obj is StavkaRacuna druga &&
+                   jelo == druga.jelo &&
+                   prilog == druga.prilog;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1228211071;
+            hashCode = hashCode * -1521134295 + jelo.GetHashCode();
+            if (prilog != null)
+            {
+                hashCode = hashCode * -1521134295 + prilog.GetHashCode();
+            }
+            return hashCode;
+        }
+
+        public static bool operator ==(StavkaRacuna left, StavkaRacuna right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(StavkaRacuna left, StavkaRacuna right)
+        {
+            return !(left == right);
+        }
+    }
+}
