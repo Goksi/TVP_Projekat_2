30c4e1c11ee43703c1dae765da5dfabe174817ca
Vukašin Lekić <goksi1312@gmail.com>
2023-06-02 14:35:57 +0200

    Update StavkaRacuna.cs

--- TVP_Projekat_2/Entiteti/StavkaRacuna.cs
+++ TVP_Projekat_2/Entiteti/StavkaRacuna.cs
@@ -15,21 +15,33 @@
 
         public int CenaJelo => jelo.Cena;
 
-        public int CenaPrilog => prilog != null ? prilog.Cena : 0;
+        public int CenaPrilog => !object.ReferenceEquals(prilog, null) ? prilog.Cena : 0;
+
+        public Jelo Jelo => jelo;
+
+        public Prilog Prilog => prilog;
 
         public override bool Equals(object obj)
         {
             if (obj == null) return false;
-            return obj is StavkaRacuna druga &&
-                   jelo == druga.jelo &&
-                   prilog == druga.prilog;
+            if (!(obj is StavkaRacuna druga)) return false;
+            bool prilogJednak;
+            if (object.ReferenceEquals(prilog, null))
+            {
+                prilogJednak = object.ReferenceEquals(druga.prilog, null);
+            }
+            else
+            {
+                prilogJednak = prilog == druga.prilog;
+            }
+            return jelo == druga.jelo && prilogJednak;
         }
 
         public override int GetHashCode()
         {
             int hashCode = -1228211071;
             hashCode = hashCode * -1521134295 + jelo.GetHashCode();
-            if (prilog != null)
+            if (!object.ReferenceEquals(prilog, null))
             {
                 hashCode = hashCode * -1521134295 + prilog.GetHashCode();
             }
